b9527601fed4368f82564a23725cec28eef40efa
Mario <pascualmedinamario@gmail.com>
2025-07-07 17:09:36 +0200

    Changes Name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -141,8 +141,8 @@ function Home({ lang, darkMode }) {
               className="button1"
               onClick={() => {
                 const link = document.createElement("a");
-                link.href = `${process.env.PUBLIC_URL}/CVMario.pdf`;
-                link.download = "CVMario.pdf";
+                link.href = `${process.env.PUBLIC_URL}/CVMARIO.pdf`;
+                link.download = "CVMARIO.pdf";
                 document.body.appendChild(link);
                 link.click();
                 document.body.removeChild(link);
