4dcd9240b3d6c398382bab8637bcb1d4a88788bc
Mario <151534424+mario1130@users.noreply.github.com>
2025-07-09 20:50:30 +0200

    Update emailConfig.js

--- src/config/emailConfig.js
+++ src/config/emailConfig.js
@@ -1,5 +1,5 @@
 export const EMAILJS_CONFIG = {
-  SERVICE_ID: "service_ti144vu",
-  TEMPLATE_ID: "template_kzn2v7o",
-  USER_ID: "KnRQLoowUot2EiW4O", 
+  SERVICE_ID: "",
+  TEMPLATE_ID: "",
+  USER_ID: "", 
 };
